79c6b9cbac2f2e09199c36b5886a69ac4fc9273c
kang <kang01105@naver.com>
2025-04-22 13:33:08 +0900

    refactor: lombok and logging, 기존코드를 롬복과 로깅으로 대체

--- src/main/java/com/example/firstproject/dto/ArticelForm.java
+++ src/main/java/com/example/firstproject/dto/ArticelForm.java
@@ -1,23 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticelForm {
     private String title;
     private String content;
-
-    public ArticelForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticelForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null,title,content);
